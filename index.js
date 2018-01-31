ee415e1910b9dd0b8b1696bebce183ce4ca2ec37
bmarchete <bmarchete@gmail.com>
2018-01-31 02:01:05 -0200

    config mysql

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const server = restify.createServer({
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    host : 'mysql://mysql:3306/',
+    host : process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',
+    port: process.env.OPENSHIFT_MYSQL_DB_PORT || 3306,
     user : 'openshift',
     password : 'mysql10102020',
     database : 'db'
